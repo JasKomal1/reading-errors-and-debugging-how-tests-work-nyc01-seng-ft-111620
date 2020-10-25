b33b9b6a44e30a5b2ad003de59bf7ccc48082a13
jsr <jaskomal2@gmail.com>
2020-10-25 20:48:01 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,10 @@ first_number = 0
 
 second_number = 1 
 
+sum = first_number + second_number
+
+difference = second_number - first_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
